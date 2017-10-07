4e283869c80b408fbf0635663e730f422b6ab198
Panfeng <qidaizhe11@gmail.com>
2017-10-07 12:14:46 +0800

    初始提交，electron-vue自动生成

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'douban-group-find-user'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
