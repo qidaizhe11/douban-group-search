f806f80156a0d435884eeae315fa6d0fee2f2182
Panfeng <qidaizhe11@gmail.com>
2017-10-07 19:38:27 +0800

    查询结果页，暂存

---
+++ src/renderer/views/Result.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="page-container">
+    <div class="filter-container">
+
+    </div>
+    <div class="tooltip-container">
+    </div>
+    <div class="table-container">
+    </div>
+    <div class="pagination-container">
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        params: {
+          type: 'group',
+          title: 'https://www.douban.com/group/mini150cm/',
+          sex: 'female',
+          city: '郑州'
+        },
+        tableData: []
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
